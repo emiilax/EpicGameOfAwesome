e27e296c1839b66f829bf649d9afcc3a5f698523
bexi <trreitmaier@msn.com>
2015-05-18 00:17:40 +0200

    levels are in correct position but input does not work

--- EGA/src/main/java/view/LevelSelect.java
+++ EGA/src/main/java/view/LevelSelect.java
@@ -29,25 +29,22 @@ public class LevelSelect extends GameState {
 	public static Sprite backgroundSprite;
 
 	private String title;
-	
-	//private GameData gd;
 
 	private int titleFontSize = 50;
 	private int menuFontSize = 50;
-	private int level;
-	private int currentItem1;
-	private String menuItems[];
+
+	private int currentRow = 0;
+	private int currentCol = 0;
+	
+	private String menuItems [][];
 
 	private GameStateManager gsm;
-	private EGATimer timer;
 	
 	public LevelSelect(GameStateManager gsm, Texture backgroundTexture){
 		super(gsm);
 		this.gsm = gsm;
 		this.backgroundTexture = backgroundTexture;
-	//	gd = SaveHandler.getGameData();
 		init();
-		
 	}
 	
 	private void init(){
@@ -62,10 +59,10 @@ public class LevelSelect extends GameState {
 
 		font = gen.generateFont(menuFontSize);
 
-		menuItems = new String[]{
-				"Level 1",
-				"Level 2",
-				"Back",
+		menuItems = new String[][]{
+				{"Level 1", "Level 2", "Level 3"}, //row 0 
+				{"Level 4", "Level 5", "Level 6"}, //row 1
+
 		};
 		
 		setTitle();
@@ -74,22 +71,38 @@ public class LevelSelect extends GameState {
 	}
 	
 	private void setTitle(){
-		title = "This is Level Select!";
+		title = "Choose level to play";
 	}
 
 	@Override
 	public void handleInput(int i) {
 		switch(i){
-		case MyInput.BUTTON_JUMP:
-			if(currentItem1 > 0){
-				currentItem1 --;
-			}
-			break;
-		case MyInput.BUTTON_DOWN:
-			if(currentItem1 < menuItems.length-1){
-				currentItem1++;
-			}
-			break;
+//		case MyInput.BUTTON_JUMP:
+//			if(currentRow > 0){
+//				currentRow --;
+//			}
+//			break;
+//		case MyInput.BUTTON_DOWN:
+//			if(currentRow < 1){ //needs to be changed if amount of rows is changed
+//				currentRow++;
+//			}
+//			break;
+		case MyInput.BUTTON_BACKWARD:
+			if(currentCol > 0){
+				if(currentCol == 0){
+					currentRow ++;
+				}else{
+					currentCol --;
+				}
+				
+			}break;
+		case MyInput.BUTTON_FORWARD:
+			if(currentCol < 3){
+				if(currentCol == 2){
+					currentRow ++;
+				}
+				currentCol ++;
+			}break;
 		case MyInput.BUTTON_ENTER:
 			select();
 			break;
@@ -97,16 +110,10 @@ public class LevelSelect extends GameState {
 	}
 	
 	private void select(){
-		if(currentItem1 == 0){ //this is selected directly if enter is pressed to long
+		String lvl = menuItems[currentRow][currentCol];
+		if(lvl == "Level 1"){
+			System.out.print("this is Level 1");
 			gsm.getGame().setLevel(new Level(gsm, gsm.getLevel(1)));
-			System.out.print("Select level 1");
-		}
-		if(currentItem1 == 1){
-			gsm.getGame().setLevel(new Level(gsm, gsm.getLevel(2)));
-			System.out.print("Select level 2");
-		} else if(currentItem1 == 2){
-			gsm.getGame().setLevel(new MenuState(gsm));
-			System.out.print("Menu");
 		}
 	}
 
@@ -131,20 +138,46 @@ public class LevelSelect extends GameState {
 
 		titleFont.draw(sb, title, (EGA.V_WIDTH-width) / 2, 600);
 
-		for(int i = 0; i < menuItems.length; i++){
-			layout.setText(font, menuItems[i]);
-			if(currentItem1 == i){
-				font.setColor(Color.RED);
-			} else {
-				font.setColor(Color.WHITE);
+		int x = menuItems[0].length;
+		
+		for (int row = 0; row < menuItems.length; row++){
+			for (int col = 0; col < x; col++){
+				layout.setText(font, menuItems[row][col]);
+				
+				if(currentRow == row && currentCol == col){
+					font.setColor(Color.RED);
+					System.out.print(menuItems[row][col]);
+				}else{
+					font.setColor(Color.WHITE);
+				}
+				
+				if(col == 0){
+					font.draw(
+							sb,
+							menuItems[row][col],
+							(EGA.V_WIDTH - width - 7*70),
+							350 - 180* row
+							);
+				}				
+				if(col == 1){
+					font.draw(
+							sb,
+							menuItems[row][col],
+							(EGA.V_WIDTH - width - 2*70 ),
+							350 - 180* row
+							);
+				}
+				if(col == 2){
+					font.draw(
+							sb,
+							menuItems[row][col],
+							(EGA.V_WIDTH - width + 3*70),
+							350 - 180* row
+							);
+				}
 			}
-			font.draw(
-					sb,
-					menuItems[i],
-					(EGA.V_WIDTH - width) / 2,
-					300 - 70 *i
-					);
 		}
+		
 		sb.end();
 		
 	}
