3b1034d1d0dc0bcd879670d645e0c41e8ffc7273
ClausX <h_ronstrom@hotmail.com>
2015-05-19 14:16:43 +0200

    added some small tests om GameData

--- EGA/src/test/java/model_test/AllModelTests.java
+++ EGA/src/test/java/model_test/AllModelTests.java
@@ -5,7 +5,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ TestCharacterModel.class, TestEntityModel.class })
+@SuiteClasses({ TestCharacterModel.class, TestEntityModel.class, TestGameData.class })
 public class AllModelTests {
 
 }
