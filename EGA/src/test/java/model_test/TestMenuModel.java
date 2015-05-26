370d694d62b0feb3073ebe332bf7d2178b7ca65f
bexi <trreitmaier@msn.com>
2015-05-26 15:11:26 +0200

    created the testClass TestMenuModel

--- EGA/src/test/java/model_test/TestMenuModel.java
+++ EGA/src/test/java/model_test/TestMenuModel.java
@@ -1,11 +1,21 @@
 package model_test;
 
 import static org.junit.Assert.*;
+
+import java.awt.Point;
+
 import model.MenuModel;
 import model.entities.EntityModel;
 
 import org.junit.Test;
 
+/**
+ * 
+ * @author Rebecka Reitmaier
+ *
+ * This class test the MenuModel
+ * It is only get and set methods
+ */
 public class TestMenuModel {
 
 	@Test
@@ -15,14 +25,89 @@ public class TestMenuModel {
 		String s = "Test Title";
 		assertTrue(s.equals(mm.getTitle()));
 	}
+	@Test
+	public void testSubTitle() {
+		MenuModel mm = new MenuModel();
+		mm.setSubTitle("Test SubTitle");
+		String s = "Test SubTitle";
+		assertTrue(s.equals(mm.getSubTitle()));
+	}
+	@Test
+	public void testTitleFontSize(){
+		MenuModel mm = new MenuModel();
+		mm.setTitleFontSize(10);
+		assertTrue(mm.getTitleFontSize()==10);
+	}
+	@Test
+	public void testMenuFontSize(){
+		MenuModel mm = new MenuModel();
+		mm.setMenuFontSize(10);
+		assertTrue(mm.getMenuFontSize()==10);
+	}
+	@Test
+	public void testSubTitleFontSize(){
+		MenuModel mm = new MenuModel();
+		mm.setSubTitleFontSize(10);
+		assertTrue(mm.getSubTitleFontSize()==10);
+	}
+	@Test
+	public void testTitleHeight(){
+		MenuModel mm = new MenuModel();
+		mm.setTitleHeight(10);
+		assertTrue(mm.getTitleHeight()==10);
+	}
 	
-//	@Test
-//	public void testSetPosition(){
-//		EntityModel em = new EntityModel();
-//		em.setPosition(3f, 4.5f);
-//		float a = em.getXPosition();
-//		float b = em.getYPosition();
-//		assertTrue(a == 3f && b == 4.5f);	
-//	}
-
+	@Test 
+	public void testMenuItemPosition(){
+		MenuModel mm = new MenuModel();
+		Point p = new Point();
+		p.x = 10;
+		p.y = 100;
+		Point [] pointArray = new Point [10];
+		pointArray[0]= p;
+		mm.setMenuItemPositions(pointArray);
+		Point [] testArray = mm.getMenuItemPositions();
+		assertTrue(testArray.equals(pointArray) && testArray[0]==pointArray[0]);
+	}
+	@Test 
+	public void testMenuItemEndPosition(){
+		MenuModel mm = new MenuModel();
+		Point p = new Point();
+		p.x = 10;
+		p.y = 100;
+		Point [] pointArray = new Point [10];
+		pointArray[0]= p;
+		mm.setMenuItemEndPositions(pointArray);
+		Point [] testArray = mm.getMenuItemEndPositions();
+		assertTrue(testArray.equals(pointArray) && testArray[0]==pointArray[0]);
+	}
+	
+	@Test 
+	public void testMenuItems(){
+		MenuModel mm = new MenuModel();
+		String [] stringArray = new String [10];
+		stringArray[0] = "testString";
+		mm.setMenuItems(stringArray);
+		String [] testArray = mm.getMenuItems();
+		assertTrue(testArray.equals(stringArray) && testArray[0]==stringArray[0]);
+	}
+	
+	@Test 
+	public void testXPos(){
+		MenuModel mm = new MenuModel();
+		mm.setXPos(10);
+		assertTrue(mm.getXPos()==10);
+	}
+	@Test 
+	public void testYPos(){
+		MenuModel mm = new MenuModel();
+		mm.setYPos(10);
+		assertTrue(mm.getYPos()==10);
+	}
+	@Test 
+	public void testGap(){
+		MenuModel mm = new MenuModel();
+		mm.setGap(10);
+		assertTrue(mm.getGap()==10);
+	}
 }
