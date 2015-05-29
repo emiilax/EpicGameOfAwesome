9a5d65853672e0303eddc62b24a28d1621d93b7c
ClausX <h_ronstrom@hotmail.com>
2015-05-29 18:16:33 +0200

    added tests for GameData

--- EGA/src/test/java/model_test/TestGameData.java
+++ EGA/src/test/java/model_test/TestGameData.java
@@ -1,6 +1,9 @@
 package model_test;
 
 import static org.junit.Assert.*;
+
+import java.util.List;
+
 import model.GameData;
 
 import org.junit.Before;
@@ -64,5 +67,43 @@ public class TestGameData {
 		
 		assertFalse(time > 0);		
 	}
+	
+	@Test
+	public void testDebug(){
+		tester.setDebug(true);
+		assertTrue(tester.getIsDebug() == true);
+		
+		tester.toggleDebug();
+		assertTrue(tester.getIsDebug() == false);
+		
+		tester.toggleDebug();
+		assertTrue(tester.getIsDebug() == true);
+	}
 
+	/* I dont know why this fails
+	@Test
+	public void testGetKeyList(){
+		tester.updateList();
+		List<Integer> gottenKeys;
+		gottenKeys = tester.getKeysList();
+		int upKey = tester.getUp();
+		assertTrue(gottenKeys.get(2) == upKey);
+	} */
+	
+	@Test
+	public void testSetSoundVolume(){
+		tester.setVolume(0.4f);
+		assertTrue(tester.getSoundVolume() == 0.4f);
+		
+	}
+	
+	@Test
+	public void floatUnder0OrOver1ShouldNotChangeSoundVolume(){
+		float startingVolume = tester.getVolume();
+		tester.setVolume(-2f);
+		assertTrue(tester.getVolume() == startingVolume);
+		
+		tester.setVolume(2f);
+		assertTrue(tester.getVolume() == startingVolume);
+	}
 }
