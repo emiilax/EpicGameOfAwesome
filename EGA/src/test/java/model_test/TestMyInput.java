52a209dd1903c80e0be9d94ea4be11cbcf6ebc30
ClausX <h_ronstrom@hotmail.com>
2015-05-30 17:07:34 +0200

    fixed some testing in MyInput

--- EGA/src/test/java/model_test/TestMyInput.java
+++ EGA/src/test/java/model_test/TestMyInput.java
@@ -3,21 +3,52 @@ package model_test;
 import static org.junit.Assert.*;
 import model.MyInput;
 
-import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 public class TestMyInput {
 
+	@Before
+	public void beforeTest(){
+		MyInput.setAllKeysFalse();
+	}
+	
 	@Test
 	public void testSetKey() {
 		int key = 2;
 		MyInput.setKey(key, true);
+		assertTrue(MyInput.keys[key] == true);
+	}
+	
+	@Test
+	public void testIsDown(){
+		int key = 2;
+		MyInput.setKey(key, true);
 		assertTrue(MyInput.isDown(key) == true);
+	}
+	
+	@Test
+	public void testIsPressed(){
+		int key = 4;
+		MyInput.setKey(key, true);
 		assertTrue(MyInput.isPressed(key) == true);
+		MyInput.update();
+		assertTrue(MyInput.isPressed(key) == false);
+		MyInput.setKey(key, false);
+		assertTrue(MyInput.isPressed(key) == false);
+	}
+	
+	@Test
+	public void testUpdateKeys(){
+		int key = 3;
+		MyInput.setKey(key, true);
+		assertTrue(MyInput.isPressed(3) == true);
+		
+		MyInput.update();
+		MyInput.setKey(key, false);
+		assertTrue(MyInput.pkeys[3] == true);
 	}
 
-	@After
-	public void afterTest(){
-		MyInput.setAllKeysFalse();
-	}
+	
+	
 }
